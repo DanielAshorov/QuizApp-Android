36349cba1c9ddbb526857c22794303de7d24365d
Adi_Levy <Adilevy94@Gmail.com>
2022-07-26 00:09:30 +0300

    dont merge!!!need to fix

---
+++ app/src/main/java/com/example/quizapp/Activities/CustomActivityAnimation.java
@@ -0,0 +1,14 @@
+package com.example.quizapp.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CustomActivityAnimation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_animation);
+    }
+}
